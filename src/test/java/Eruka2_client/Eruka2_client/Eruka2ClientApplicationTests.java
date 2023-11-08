31800b6e515aee5b8b612a435d5d58b4f6c07a79
Premkumar0000 <premthadi25@gmail.com>
2023-11-08 14:17:12 +0530

    first msg

---
+++ src/test/java/Eruka2_client/Eruka2_client/Eruka2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package Eruka2_client.Eruka2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eruka2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
